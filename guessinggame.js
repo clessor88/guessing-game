0127aba96a422ee75496658d0988d235357e9fec
Crystal Lessor <clessor88@yahoo.com>
2015-11-02 15:26:15 -0800

    first commit

---
+++ guessinggame.js
@@ -0,0 +1,27 @@
+		confirm("Are you ready to play?")
+		var color=prompt("Whats my favorite color?");
+			if(color == "pink")
+			{
+    		 alert("Nailed it! My favorite color is pink.");
+			}
+			else {
+   			 alert("Wrong, my favorite color is pink");
+			}
+
+		var state=prompt("What state was I born in?");
+			if(state=== "Washington") 
+			 {
+			 alert("Correct! I was born in Washington");
+			}
+			else {
+				alert("Incorrect, I was born in Washington")
+			}
+	 	var food=prompt("Do I love seafood?");
+	 		if(food="yes")
+	 		{
+	 		 alert("YES! I love seafood!")
+	 		}
+	 		else 
+	 		{
+	 			alert("Wrong. I love seafood.")
+	 		}
