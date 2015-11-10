88c5ef9c50d5b28dd36b7285061aa3faa419234f
Crystal Lessor <clessor88@yahoo.com>
2015-11-09 16:46:53 -0800

    new commit guessing game

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # guessing-game
+
+If you want to get the guessing game questions correct, youll need to read the previously submitted about-me page!
+The answers are in there for you :)
+
+
+How many programmers does it take to change a light bulb?
+None – It’s a hardware problem 
--- guessinggame.js
+++ guessinggame.js
@@ -1,27 +1,18 @@
-		confirm("Are you ready to play?")
-		var color=prompt("Whats my favorite color?");
-			if(color == "pink")
-			{
-    		 alert("Nailed it! My favorite color is pink.");
-			}
-			else {
-   			 alert("Wrong, my favorite color is pink");
-			}
+confirm("Are you ready to play?")
+	var color=prompt("Whats my favorite color?");
+		document.getElementById('favColor').innerHTML = 'Your answer was ' + color + '. The Correct answer is pink!';
 
-		var state=prompt("What state was I born in?");
-			if(state=== "Washington") 
-			 {
-			 alert("Correct! I was born in Washington");
-			}
-			else {
-				alert("Incorrect, I was born in Washington")
-			}
-	 	var food=prompt("Do I love seafood?");
-	 		if(food="yes")
-	 		{
-	 		 alert("YES! I love seafood!")
-	 		}
-	 		else 
-	 		{
-	 			alert("Wrong. I love seafood.")
-	 		}
+	var state=prompt("What state was I born in?");
+		document.getElementById('homeState').innerHTML = 'Your answer was ' + state + '. The correct answer is Washington!';
+
+    var food=prompt("Do I love seafood?");
+	 	document.getElementById('favFood').innerHTML = 'Your answer was ' + food + '. The correct answer is yes!';
+
+	var years=prompt('How many years did I live in South Carolina?');
+		document.getElementById('manyYears').innerHTML = 'Your answer was ' + years + '. The correct answer is 6!';
+
+	var kitties=prompt('Do I like kitties?');
+		document.getElementById('softKitty').innerHTML = 'Your answer was ' + kitties + '. The correct answer is YES!';
+
+	var date=prompt('What year did I move back from Washington?');
+		document.getElementById('homeDate').innerHTML = 'Your answer was ' + date + '. The answer is 2014!';
